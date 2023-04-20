b1f6c965ecd97b5131acf75d1985447a53ed6b0e
andreamastro98 <andreamastrovito98@gmail.com>
2023-04-20 11:56:57 +0200

    struttura html creata

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+const { createApp } = Vue
+
+  createApp({
+    data() {
+      return {
+        //variabili vue 3
+        message: 'Hello Vue!',
+      }
+    },
+    methods: {
+        //funzioni vue 3
+        
+    }
+  }).mount('#app')
